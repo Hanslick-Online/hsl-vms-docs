41ef74aca1d8b87f8b4340e829601d02a0f3f050
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2023-02-01 15:47:20 +0100

    minor

--- fetch_data.sh
+++ fetch_data.sh
@@ -6,6 +6,8 @@ unzip main
 mkdir ./data
 mkdir ./data/editions
 mkdir ./data/tmp
+mkdir ./data/meta
+touch ./data/meta/.gitkeep
 mv hsl-transkribus-export-main/tei/162553/*.xml ./data/tmp
 rm -rf hsl-transkribus-export-main
 rm main.zip
