6ebd1ea25021d574d7c4273b9feb805970b754f6
Fernando Sanz-Lázaro <fsanzl@gmail.com>
2025-06-03 14:55:42 +0200

    ul

--- scripts/sh/upload_img_iiif.sh
+++ scripts/sh/upload_img_iiif.sh
@@ -1,10 +1,10 @@
 # bin/bash
 
 # make nested directory in the root upload (images) directory 
-# curl --anyauth --user $CRED -k -X MKCOL 'https://iiif.acdh.oeaw.ac.at/upload/hsl-nfp' 
+# curl --anyauth --user $IIIF_CRED -k -X MKCOL 'https://iiif.acdh.oeaw.ac.at/upload/hsl-doc' 
 
 # add local dir with images for upload
-cd ./new_facs && find . -exec curl -T {} 'https://iiif.acdh.oeaw.ac.at/upload/hsl-nfp/{}' --user $CRED -k \;
+cd ./data/facs && find . -exec curl -T {} 'https://iiif.acdh.oeaw.ac.at/upload/hsl-doc/{}' --user $IIIF_CRED -k \;
 
 # delete images
-# curl --anyauth --user $CRED -X "DELETE" "https://iiif.acdh.oeaw.ac.at/upload/hsl-nfp/"
+# curl --anyauth --user $IIIF_CRED -X "DELETE" "https://iiif.acdh.oeaw.ac.at/upload/hsl-doc/"
