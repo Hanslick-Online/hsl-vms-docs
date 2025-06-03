542e8850e021e9dbe7d9137af05070488e592085
Fernando Sanz-Lázaro <fsanzl@gmail.com>
2025-06-03 14:47:22 +0200

    rename

--- scripts/sh/fix_xml_pics.sh
+++ scripts/sh/fix_xml_pics.sh
@@ -9,9 +9,8 @@ grep -E 'jpe?g' data/tmp/* | sed -n 's/.*url="\([^"]*jpe\?g\)".*/\1/p' | while I
     # Normalize filename: .jpeg → .jpg, replace spaces, reformat
     new_name=$(echo "$file_name" | \
         sed 's/.jpeg$/.jpg/' | \
-        sed -E 's/^Hanslick (.+) ([0-9]+-[0-9]+\.jpg)$/Hanslick_\1_\2/' | \
-        tr -d ' ')
-    sed -i "s|$file_name|d__$new_name|g" data/tmp/*xml
+	sed -E 's/^Hanslick (.+) ([0-9]+-[0-9]+\.jpg)$/Hanslick_\1_\2/' | sed -E 's/^([0-9]*) */d_\1_/' | tr -d ' ')
+    sed -i "s|$file_name|$new_name|g" data/tmp/*xml
 done
 
 #sed -i -E 's/(Hanslick_.+\.jpg)/d__\1/g' data/editions/*
